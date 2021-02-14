27cb32c99f90b6b3f56233d23677e6a23f4afd3d
tvps20 <thiago.vps20@gmail.com>
2021-02-14 18:37:04 -0300

    first commit

---
+++ src/main/java/br/com/santiago/ccl/CclApplication.java
@@ -0,0 +1,13 @@
+package br.com.santiago.ccl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/santiago/ccl/CclApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santiago.ccl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
